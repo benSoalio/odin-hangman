cc14d613414e17efa95b0ef0bf8c1c00f3124a85
bsoalio <bsoalio@gmail.com>
2022-09-01 22:17:09 +0000

    Modify game class

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,29 @@
 require_relative 'game'
+choice = 0
+puts <<-WELCOME
+  Welcome to hangman by COZAKH
+WELCOME
 
-puts 'Welcome to hangman by Cozakh'
+# Generate a secret word
+def generate_secret_word(filename)
+  # Read the google english no swear and keep the word between 5 and 12 characters
+  words = File.readlines(filename).select { |word| word.length > 5 && word.length < 13}
+  # Randomly select a word
+  words.sample
+end
 
-Game.new
+secret_word = generate_secret_word('google-10000-english-no-swears.txt').chomp
+guess_word = ''
+secret_word.each_char { |char| guess_word << '_'}
+wrong_guesses = []
+number_of_guess = 10
 
+
+until choice == 1 || choice == 2
+  puts 'press 1 to play a new game'
+  puts 'press 2 to load a saved game'
+  choice = gets.chomp.to_i
+end
+
+
+Game.new(secret_word, guess_word, wrong_guesses, number_of_guess)
